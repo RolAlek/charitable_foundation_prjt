03905831a8620ab8ba013abdb041cdf5de90cf78
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-02-26 12:42:26 +0900

    tets #2

--- app/core/config.py
+++ app/core/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
+    # Google API
     type: Optional[str] = None
     project_id: Optional[str] = None
     private_key_id: Optional[str] = None
